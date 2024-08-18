c41e927755a20dc463018bec5223a119d6ace7e8
baraa <baraa.aburas@gmail.com>
2024-08-18 12:11:33 +0300

    Add Products, View Models

---
+++ Bulky.DataAccess/Migrations/20240818071810_AddImageToPruduct.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bulky.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImageToPruduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageURL",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageURL",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageURL",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageURL",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ImageURL",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ImageURL",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ImageURL",
+                value: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageURL",
+                table: "Products");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,114 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Bulky.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BulkyWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            List<Product> products = _unitOfWork.Product.GetAll().ToList();
+            return View(products);
+        }
+        public IActionResult Create()
+        {
+            ProductVM productVM = new()
+            {
+                CategoryList = _unitOfWork.Category
+               .GetAll().Select(u => new SelectListItem
+               {
+                   Text = u.Name,
+                   Value = u.Id.ToString()
+               }),
+            Product = new Product()
+            };
+            return View(productVM);
+        }
+    
+
+        
+        [HttpPost]
+        public IActionResult Create(ProductVM productVM)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(productVM.Product);
+                _unitOfWork.Save();
+                TempData["success"] = "Product Created Successfully";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+
+                productVM.CategoryList = _unitOfWork.Category
+                         .GetAll().Select(u => new SelectListItem
+                         {
+                             Text = u.Name,
+                             Value = u.Id.ToString()
+                         });
+         
+            return View(productVM);
+            }
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product? product = _unitOfWork.Product.Get(o => o.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(product);
+                _unitOfWork.Save();
+                TempData["success"] = "Product Updated Successfully";
+                return RedirectToAction("index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product product = _unitOfWork.Product.Get(o => o.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Product product = _unitOfWork.Product.Get(o => o.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Product.Remove(product);
+            _unitOfWork.Save();
+            TempData["success"] = "Product Deleted Successfully";
+            return RedirectToAction("index");
+        }
+    }
+}
